f9cdc9e7c33cb7eb2aa2cb15d621b3d405f66426
j4qfrost <j4qfrost@gmail.com>
2017-09-05 05:15:35 -0700

    Update dgvoice.go

--- dgvoice.go
+++ dgvoice.go
@@ -29,8 +29,8 @@ import (
 // These below values seem to provide the best overall performance
 const (
 	channels  int = 1                   // 1 for mono, 2 for stereo
-	frameRate int = 16000               // audio sampling rate
-	frameSize int = 512                 // uint16 size of each audio frame
+	frameRate int = 48000               // audio sampling rate
+	frameSize int = 960                 // uint16 size of each audio frame
 	maxBytes  int = (frameSize * 2) * 2 // max size of opus data
 )
 
